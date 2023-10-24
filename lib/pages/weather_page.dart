97b870bb3c2c50d5a90b37572736606e49cd1c84
Loïc <105038676+MayranL@users.noreply.github.com>
2023-10-24 09:16:34 +0200

    Add weather_models and weather_services

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meteo_app/services/weather_services.dart';
+
+import '../models/weather_model.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
